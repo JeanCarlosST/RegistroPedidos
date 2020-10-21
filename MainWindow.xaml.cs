c5352b9ee1e2b1a9c8bfcbd48ba425df75fe1f05
Kleyton72 <jeanc.1582@gmail.com>
2020-10-21 00:02:13 -0400

    Agregando MainWindow

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using RegistroPedidos.UI.Registro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RegistroPedidos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void rOrdenesMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            new rOrdenes().Show();
+        }
+
+        public void cOrdenesMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void cProductosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public void cSuplidoresMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
