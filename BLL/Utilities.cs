3ffc408aac1222991bd54341d4646067f21234b4
Kleyton72 <jeanc.1582@gmail.com>
2020-10-24 10:47:38 -0400

    Agregando función ToFloat en Utilities

--- BLL/Utilities.cs
+++ BLL/Utilities.cs
@@ -11,6 +11,15 @@ namespace RegistroPedidos.BLL
             return return_;
         }
 
+        public static float ToFloat(string value)
+        {
+            float return_ = 0;
+
+            float.TryParse(value, out return_);
+
+            return return_;
+        }
+
         public static decimal ToDecimal(string value)
         {
             decimal return_ = 0;
